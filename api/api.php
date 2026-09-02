f1798c821ff919c017f88781e7ad2bf11e453376
KML M <kamelking2028@gmail.com>
2026-09-02 19:24:50 +0200

    Update api.php

--- api/api.php
+++ api/api.php
@@ -2,6 +2,8 @@
 header("Access-Control-Allow-Origin: https://kamelking2028-coder.github.io");
 header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Access-Control-Allow-Origin: *");
+
 
 if ($_SERVER["REQUEST_METHOD"] === "OPTIONS") {
     http_response_code(200);
